bc205bb2ac0bac466ee67ab29d38ce08e0643ce4
AltKorin <konradkomorowski88@gmail.com>
2025-03-15 19:10:31 +0100

    Konfiguracja pod Heroku14

--- KamieniarstwoOrganiser/KamieniarstwoOrganiser/settings.py
+++ KamieniarstwoOrganiser/KamieniarstwoOrganiser/settings.py
@@ -18,7 +18,7 @@ DATABASES['default'] = dj_database_url.config(
 # Statyczne pliki
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'orders' / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Klucz bezpieczeństwa, DEBUG, ALLOWED_HOSTS, INSTALLED_APPS itd.
@@ -131,12 +131,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
-# Konfiguracja plików statycznych
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
